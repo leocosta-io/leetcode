c6fa077a09a0c3baf06d993a5b9bdc926b98fb49
Leonardo Costa <me@leocosta.io>
2022-09-07 23:55:43 -0300

    Time: 116 ms (30.99%), Space: 13.9 MB (59.53%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,13 +1,17 @@
+# class Solution:
+#     def isPalindrome(self, x: int) -> bool:
+#         x_str = str(x)
+#         i = 0
+        
+#         while i <= len(x_str)/2:
+#             if x_str[i] != x_str[(i+1)*-1]:
+#                 return False
+            
+#             i += 1
+            
+#         return True
+
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x_str = str(x)
-        i = 0
-        
-        while i <= len(x_str)/2:
-            if x_str[i] != x_str[(i+1)*-1]:
-                return False
-            
-            i += 1
-            
-        return True            
-        
+        s = str(x)
+        return s == s[::-1]
