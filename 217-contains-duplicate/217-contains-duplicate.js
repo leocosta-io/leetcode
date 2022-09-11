61f8838ec9c8ac3b9b9c34628c10be38f20784ef
Leonardo Costa <me@leocosta.io>
2022-09-11 09:04:44 -0300

    Time: 1002 ms (14.76%), Space: 50 MB (86.72%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -2,13 +2,34 @@
  * @param {number[]} nums
  * @return {boolean}
  */
+
 var containsDuplicate = function(nums) {
-    unique = new Set(nums);
+    let uniques = [];
+    
+    while (nums.length > 0) {
         
-    if (unique.size == nums.length) {
-        return false
+        a = nums.pop();
+        
+        for (let o=0; o<uniques.length; o++) {
+            if (a == uniques[o]) {
+                return true;
+            }
+        }
+        
+        uniques.push(a);
     }
-    else {
-        return true
-    }
-};
+        
+    return false;
+}
+
+// previous solution
+// var containsDuplicate = function(nums) {
+//     unique = new Set(nums);
+        
+//     if (unique.size == nums.length) {
+//         return false
+//     }
+//     else {
+//         return true
+//     }
+// };
