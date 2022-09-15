8aa1fb144816a3ec92261e667133b9bb380261f6
Leonardo Costa <me@leocosta.io>
2022-09-14 21:33:58 -0300

    Time: 83 ms (63.29%), Space: 42.1 MB (60.43%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.js
+++ 35-search-insert-position/35-search-insert-position.js
@@ -8,15 +8,14 @@ var searchInsert = function(nums, target) {
     let higher = nums.length-1;
     let i = Math.round((higher-bottom)/2);
     
-    console.log("target: ", target);
+    // console.log("target: ", target);
     
     while (target != nums[i] && higher-bottom>1) {
-    // while (target != nums[i] && higher!=bottom) {
-        console.log();
-        console.log("bottom: ", bottom);
-        console.log("higher: ", higher);
-        console.log("nums[i]: ", nums[i]);
-        console.log("i: ", i);
+        // console.log();
+        // console.log("bottom: ", bottom);
+        // console.log("higher: ", higher);
+        // console.log("nums[i]: ", nums[i]);
+        // console.log("i: ", i);
         
         
         if (target > nums[i]) {
@@ -26,11 +25,11 @@ var searchInsert = function(nums, target) {
             higher = i;
         }
         
-        // i = Math.round((higher-bottom)/2 + bottom);
         i = Math.round((higher-bottom)/2) + bottom;
-        console.log("i after round: ", i);
-        console.log("bottom after round: ", bottom);
-        console.log("higher after round: ", higher);
+        
+        // console.log("i after round: ", i);
+        // console.log("bottom after round: ", bottom);
+        // console.log("higher after round: ", higher);
     }
     
     if (target <= nums[bottom]) {
