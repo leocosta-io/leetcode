430be12030d2b6e04f71abe5d7643c1546019a6e
Leonardo Costa <me@leocosta.io>
2022-09-15 21:03:30 -0300

    Time: 105 ms (71.47%), Space: 42.7 MB (92.83%) - LeetHub

--- 69-sqrtx/69-sqrtx.js
+++ 69-sqrtx/69-sqrtx.js
@@ -2,12 +2,23 @@
  * @param {number} x
  * @return {number}
  */
+
+// var mySqrt = function(x) {
+//     let i=1;
+//     while (i*i<=x) {
+//         i++;
+//     }
+//     return --i;
+// };
+
 var mySqrt = function(x) {
     let i=1;
+    let sqrt = 0;
     
-    while (i*i<=x) {
-        i++;
+    while (x-i >= 0) {
+        x = x - i;
+        i = i + 2;
+        sqrt++;
     }
-    
-    return --i;
+    return sqrt;
 };
