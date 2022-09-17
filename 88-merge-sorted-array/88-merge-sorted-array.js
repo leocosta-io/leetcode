a5f56932aa65681e7aacd9b5e89d297b68f5d5db
Leonardo Costa <me@leocosta.io>
2022-09-17 11:57:42 -0300

    Time: 427 ms (5.26%), Space: 50.6 MB (5.01%) - LeetHub

---
+++ 88-merge-sorted-array/88-merge-sorted-array.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let ind1 = 0;
+    let ind2 = 0;
+    
+    while (ind2 < n) {
+        console.log("nums1: ", nums1);
+        console.log("ind1: ", nums1[ind1]);
+        console.log("nums2: ", nums2);
+        console.log("ind2: ", nums2[ind2]);
+        console.log();
+        
+        if (nums2[ind2] <= nums1[ind1]) {
+            nums1.splice(ind1, 0, nums2[ind2]);
+            nums1.pop();
+            m++;
+            ind2++;
+        } 
+        else if (ind1 == m) {
+            nums1.splice(ind1, 0, nums2[ind2]);
+            nums1.pop();
+            m++;
+            ind2++;
+        }
+        
+        ind1++;
+    }
+    
+    return nums1;
+    
+};
