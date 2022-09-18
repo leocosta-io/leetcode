b73fe0eb9261221d2d15e3abb59c0358da74a4b2
Leonardo Costa <me@leocosta.io>
2022-09-18 16:12:13 -0300

    Time: 6025 ms (5.02%), Space: 47.7 MB (30.42%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -7,19 +7,30 @@ var isAnagram = function(s, t) {
     let s_ind = 0;
     let t_ind = 0;
     
+    s = [...s];
+    t = [...t];
+    
     if (s.length !== t.length) {
         return false;
     }
     
+    // console.log("s: ", s);
     while (s_ind < s.length) {      
+        // console.log("s_ind: ", s[s_ind]);
+        // console.log("t: ", t);
+        // console.log();
+        
         t_ind = 0;
         
-        while (s.charAt(s_ind) !== t.charAt(t_ind) && t_ind < t.length) {
+        // while (s.charAt(s_ind) !== t.charAt(t_ind) && t_ind < t.length) {
+        while (s[s_ind] !== t[t_ind] && t_ind < t.length) {
             t_ind++;
         }
         
-        if (s.charAt(s_ind) === t.charAt(t_ind)) {
-            t = t.slice(0, t_ind) + t.slice(t_ind+1);
+        // if (s.charAt(s_ind) === t.charAt(t_ind)) {
+        if (s[s_ind] === t[t_ind]) {
+            // t = t.slice(0, t_ind) + t.slice(t_ind+1);
+            t.splice(t_ind, 1);
         }
         else {
             return false;
